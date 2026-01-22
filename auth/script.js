4afbee7a50d50731a85ebb2af49a02d34dcae18c
suruchi Bharti <suruchi.bharti@intimetec.com>
2026-01-22 16:12:28 +0530

    initial_commit

---
+++ auth/script.js
@@ -0,0 +1,72 @@
+let registerBtn = document.querySelector('.registerbtn');
+let signInBtn = document.querySelector('.signinbtn');
+let nameField = document.querySelector('.namefield');
+let title = document.querySelector('.title');
+let underline = document.querySelector('.underline');
+let text = document.querySelector('.text');
+
+let isLogin = false;
+
+
+signInBtn.addEventListener('click', ()=>{
+    isLogin = true;
+
+    nameField.style.maxHeight ='0';
+    title.innerHTML = 'Sign In';
+    text.innerHTML='Lost Password';
+    registerBtn.classList.add('disable');
+    signInBtn.classList.remove('disable');
+    underline.style.transform = 'translateX(35px)';
+});
+
+
+registerBtn.addEventListener('click', ()=>{
+    isLogin = false;
+
+    nameField.style.maxHeight ='60px';
+    title.innerHTML = 'Register';
+    text.innerHTML='Password Suggestions';
+    signInBtn.classList.add('disable');
+    registerBtn.classList.remove('disable');
+    underline.style.transform = 'translateX(0)';
+});
+
+registerBtn.addEventListener('click', ()=>{
+    if(!isLogin){ 
+        let nameVal = document.getElementById("name").value.trim();
+        let emailVal = document.getElementById("email").value.trim();
+        let passVal = document.getElementById("password").value.trim();
+
+        if(!nameVal || !emailVal || !passVal){
+            alert("Fill all fields");
+            return;
+        }
+
+        localStorage.setItem("name", nameVal);
+        localStorage.setItem("email", emailVal);
+        localStorage.setItem("password", passVal);
+
+        alert("Registered Successfully. Now Sign In");
+
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("password").value="";
+    }
+});
+
+signInBtn.addEventListener('click', ()=>{
+    isLogin = true;
+
+    let emailVal = document.getElementById("email").value.trim();
+    let passVal = document.getElementById("password").value.trim();
+
+    let se = localStorage.getItem("email");
+    let sp = localStorage.getItem("password");
+
+    if(emailVal === se && passVal === sp){
+        window.location = "../gallery/index.html"; 
+    }else{
+        alert("Invalid Email or Password");
+    }
+});
+
---
+++ gallery/script.js
@@ -0,0 +1,30 @@
+let employees=[];
+let index=0;
+
+let user = localStorage.getItem("name");
+welcomeUser.innerText = "Welcome " + user;
+
+fetch("employees.json")
+.then(res=>res.json())
+.then(data=>{
+  employees=data;
+  showEmployee(0);
+});
+
+function showEmployee(i){
+  empImg.src=employees[i].image;
+  empName.innerText=employees[i].name;
+  empDes.innerText=employees[i].designation;
+  empProj.innerText=employees[i].project;
+  empExp.innerText=employees[i].experience;
+}
+
+function next(){
+  index=(index+1)%employees.length;
+  showEmployee(index);
+}
+
+function prev(){
+  index=(index-1+employees.length)%employees.length;
+  showEmployee(index);
+}
