e8b2b20e06499cbce547709b4a5b31607347741f
suruchi Bharti <suruchi.bharti@intimetec.com>
2026-02-06 12:05:23 +0530

    validation added for image and changes in genre

--- React_node_week1_Assignment/Movie_list_App/src/components/MovieForm.tsx
+++ React_node_week1_Assignment/Movie_list_App/src/components/MovieForm.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import type { Movie } from "../types/Movie";
 
+function isValidImageUrl(url: string) {
+  return /\.(jpeg|jpg|png|gif|webp)$/i.test(url);
+}
+
 const MovieForm = ({ onAdd }: { onAdd: (m: Omit<Movie, "id">) => void }) => {
   const [movie, setMovie] = useState<Omit<Movie, "id">>({
     title: "",
@@ -11,18 +15,23 @@ const MovieForm = ({ onAdd }: { onAdd: (m: Omit<Movie, "id">) => void }) => {
   });
 
   const handleAdd = () => {
-  
+
     if (!movie.title || !movie.genre || !movie.poster || !movie.year || !movie.rating) {
       alert("Please fill all fields before adding the movie.");
       return;
     }
 
+    if (!isValidImageUrl(movie.poster)) {
+      alert("Please enter a valid image URL (jpg, png, jpeg, gif, webp).");
+      return;
+    }
+
     onAdd(movie);
 
-    
+
     setMovie({
       title: "",
-      year:2026,
+      year: 2026,
       rating: 10,
       genre: "",
       poster: ""
@@ -53,11 +62,16 @@ const MovieForm = ({ onAdd }: { onAdd: (m: Omit<Movie, "id">) => void }) => {
         value={movie.rating}
         onChange={e => setMovie({ ...movie, rating: +e.target.value })}
       />
-      <input
-        placeholder="Genre"
+      <select
         value={movie.genre}
         onChange={e => setMovie({ ...movie, genre: e.target.value })}
-      />
+      >
+        <option value="">Select Genre</option>
+        <option value="Sci-Fi">Sci-Fi</option>
+        <option value="Action">Action</option>
+        <option value="Drama">Drama</option>
+        <option value="Romance">Romance</option>
+      </select>
       <button onClick={handleAdd}>Add Movie</button>
     </div>
   );
