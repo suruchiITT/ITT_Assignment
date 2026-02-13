f51f7ec5c273cfdfee164d179e2ea70dd56390c7
suruchi Bharti <suruchi.bharti@intimetec.com>
2026-02-13 19:21:39 +0530

    schema added for user and post

---
+++ React_node_week3/backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    _id: {
+        type: String,
+        required: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    profilePic: {
+        type: String,
+        default: null
+    },
+
+    following: [String],
+
+    followers: [String]
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
